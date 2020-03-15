321f54c7b6e4e1707f8020a00f5324eadc7bd618
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-15 20:39:07 +0000

    Created AddUser, User and UserList components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-/*
-This exercise will help you put together and practice all of the concepts you've
-learned thus far. It will also help you form a strong foundational knowledge of
-React and prepare you for your first project.
+import AddUSer from './AddUser';
+import UserList from './UserList';
 
-The instructions for this project are located in the `instructions.md` file.
-*/
+
 
 class App extends Component {
+  state = {
+    users
+  };
   render() {
     return (
       <div className="App">
